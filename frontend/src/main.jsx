9b0d604ef4c2eea209f28a8bab407cb43f472121
Benoît Vandanjon <benoit@vandanjon.fr>
2023-10-30 14:37:14 +0100

    base router operations

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,22 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 const router = createBrowserRouter([
   {
-    path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
   },
 ]);
 
---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,11 @@
+function About() {
+  return (
+    <div id="AboutContainer">
+      <h1>About</h1>
+
+      <p>About Page</p>
+    </div>
+  );
+}
+
+export default About;
